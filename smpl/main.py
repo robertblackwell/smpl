60e27242721c808bf9e9699e09cf310e30b16f41
Robert Blackwell <rob@whiteacorn.com>
2021-10-01 16:29:38 +1000

    Bump version: 0.10.1 → 1.0.0

--- smpl/main.py
+++ smpl/main.py
@@ -13,7 +13,7 @@ pp = pprint.PrettyPrinter(indent=4)
 debug = True
 logfile = False
 
-__version__ = "0.10.1"
+__version__ = "1.0.0"
 
 def main():
     parser = cli_interface.define_cli_interface()
